67a54f99b7e6ae19465ebe7dc2fb5c2c235e70cd
harsh11101 <hiteshpant11101@gmail.com>
2023-11-27 14:09:29 +0530

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import Dropdown from '../Dropdown/Dropdown'
 import './Navbar.css'
 
-export default function Navbar({setGroup, setOrder, isBoxVisible, setIsBoxVisible}) {
+export default function Navbar(props) {
+  const {setGroup, setOrder, ...restProps} = props;
   return (
     <div id="navbar">
         <Dropdown setGroup={setGroup} setOrder={setOrder} />
